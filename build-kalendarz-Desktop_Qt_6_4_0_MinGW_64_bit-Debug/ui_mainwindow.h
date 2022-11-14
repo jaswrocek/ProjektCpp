6ff0a3d4f31d91ce25947a66bbfcfc58a145799f
JanKoza <jaswrocek@gmail.com>
2022-11-15 00:38:04 +0100

    Pierwszy commit

---
+++ build-kalendarz-Desktop_Qt_6_4_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,498 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.4.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionstrza_ka_w_gore;
+    QAction *actionstrza_ka_w_d;
+    QAction *actionmiesiac_i_rok;
+    QWidget *centralwidget;
+    QGridLayout *gridLayout_6;
+    QGridLayout *gridLayout_2;
+    QWidget *widget_34;
+    QWidget *widget_18;
+    QWidget *widget_20;
+    QWidget *widget_29;
+    QWidget *widget_10;
+    QWidget *widget_19;
+    QWidget *widget_4;
+    QWidget *widget_13;
+    QWidget *widget_27;
+    QWidget *widget_28;
+    QWidget *widget_17;
+    QWidget *widget_5;
+    QWidget *widget_3;
+    QWidget *widget_22;
+    QWidget *widget_25;
+    QWidget *widget_9;
+    QWidget *widget_21;
+    QWidget *widget_33;
+    QWidget *widget;
+    QWidget *widget_2;
+    QWidget *widget_26;
+    QWidget *widget_35;
+    QWidget *widget_12;
+    QWidget *widget_7;
+    QWidget *widget_24;
+    QWidget *widget_6;
+    QWidget *widget_23;
+    QWidget *widget_32;
+    QWidget *widget_31;
+    QWidget *widget_15;
+    QWidget *widget_16;
+    QWidget *widget_30;
+    QWidget *widget_8;
+    QWidget *widget_11;
+    QWidget *widget_14;
+    QLabel *label_4;
+    QLabel *label_7;
+    QLabel *label_6;
+    QLabel *label_5;
+    QLabel *label_3;
+    QLabel *label_2;
+    QLabel *label;
+    QMenuBar *menubar;
+    QMenu *menumenu;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(800, 800);
+        actionstrza_ka_w_gore = new QAction(MainWindow);
+        actionstrza_ka_w_gore->setObjectName("actionstrza_ka_w_gore");
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/rec/C:/Users/jaswr/Downloads/strzalkawgore.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionstrza_ka_w_gore->setIcon(icon);
+        actionstrza_ka_w_d = new QAction(MainWindow);
+        actionstrza_ka_w_d->setObjectName("actionstrza_ka_w_d");
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/rec/C:/Users/jaswr/Downloads/pobrane.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionstrza_ka_w_d->setIcon(icon1);
+        actionmiesiac_i_rok = new QAction(MainWindow);
+        actionmiesiac_i_rok->setObjectName("actionmiesiac_i_rok");
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        gridLayout_6 = new QGridLayout(centralwidget);
+        gridLayout_6->setObjectName("gridLayout_6");
+        gridLayout_2 = new QGridLayout();
+        gridLayout_2->setObjectName("gridLayout_2");
+        widget_34 = new QWidget(centralwidget);
+        widget_34->setObjectName("widget_34");
+        widget_34->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_34, 5, 5, 1, 1);
+
+        widget_18 = new QWidget(centralwidget);
+        widget_18->setObjectName("widget_18");
+        widget_18->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_18, 3, 1, 1, 1);
+
+        widget_20 = new QWidget(centralwidget);
+        widget_20->setObjectName("widget_20");
+        widget_20->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_20, 3, 3, 1, 1);
+
+        widget_29 = new QWidget(centralwidget);
+        widget_29->setObjectName("widget_29");
+        widget_29->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_29, 4, 6, 1, 1);
+
+        widget_10 = new QWidget(centralwidget);
+        widget_10->setObjectName("widget_10");
+        widget_10->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_10, 1, 6, 1, 1);
+
+        widget_19 = new QWidget(centralwidget);
+        widget_19->setObjectName("widget_19");
+        widget_19->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_19, 3, 2, 1, 1);
+
+        widget_4 = new QWidget(centralwidget);
+        widget_4->setObjectName("widget_4");
+        widget_4->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_4, 2, 2, 1, 1);
+
+        widget_13 = new QWidget(centralwidget);
+        widget_13->setObjectName("widget_13");
+        widget_13->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_13, 5, 0, 1, 1);
+
+        widget_27 = new QWidget(centralwidget);
+        widget_27->setObjectName("widget_27");
+        widget_27->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_27, 4, 4, 1, 1);
+
+        widget_28 = new QWidget(centralwidget);
+        widget_28->setObjectName("widget_28");
+        widget_28->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_28, 4, 5, 1, 1);
+
+        widget_17 = new QWidget(centralwidget);
+        widget_17->setObjectName("widget_17");
+        widget_17->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_17, 2, 6, 1, 1);
+
+        widget_5 = new QWidget(centralwidget);
+        widget_5->setObjectName("widget_5");
+        widget_5->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_5, 2, 1, 1, 1);
+
+        widget_3 = new QWidget(centralwidget);
+        widget_3->setObjectName("widget_3");
+        widget_3->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_3, 1, 2, 1, 1);
+
+        widget_22 = new QWidget(centralwidget);
+        widget_22->setObjectName("widget_22");
+        widget_22->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_22, 3, 5, 1, 1);
+
+        widget_25 = new QWidget(centralwidget);
+        widget_25->setObjectName("widget_25");
+        widget_25->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_25, 4, 2, 1, 1);
+
+        widget_9 = new QWidget(centralwidget);
+        widget_9->setObjectName("widget_9");
+        widget_9->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_9, 1, 5, 1, 1);
+
+        widget_21 = new QWidget(centralwidget);
+        widget_21->setObjectName("widget_21");
+        widget_21->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_21, 3, 4, 1, 1);
+
+        widget_33 = new QWidget(centralwidget);
+        widget_33->setObjectName("widget_33");
+        widget_33->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_33, 5, 4, 1, 1);
+
+        widget = new QWidget(centralwidget);
+        widget->setObjectName("widget");
+        widget->setLayoutDirection(Qt::LeftToRight);
+        widget->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget, 1, 0, 1, 1);
+
+        widget_2 = new QWidget(centralwidget);
+        widget_2->setObjectName("widget_2");
+        widget_2->setLayoutDirection(Qt::LeftToRight);
+        widget_2->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_2, 1, 1, 1, 1);
+
+        widget_26 = new QWidget(centralwidget);
+        widget_26->setObjectName("widget_26");
+        widget_26->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_26, 4, 3, 1, 1);
+
+        widget_35 = new QWidget(centralwidget);
+        widget_35->setObjectName("widget_35");
+        widget_35->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_35, 5, 6, 1, 1);
+
+        widget_12 = new QWidget(centralwidget);
+        widget_12->setObjectName("widget_12");
+        widget_12->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_12, 4, 0, 1, 1);
+
+        widget_7 = new QWidget(centralwidget);
+        widget_7->setObjectName("widget_7");
+        widget_7->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_7, 1, 3, 1, 1);
+
+        widget_24 = new QWidget(centralwidget);
+        widget_24->setObjectName("widget_24");
+        widget_24->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_24, 4, 1, 1, 1);
+
+        widget_6 = new QWidget(centralwidget);
+        widget_6->setObjectName("widget_6");
+        widget_6->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_6, 2, 0, 1, 1);
+
+        widget_23 = new QWidget(centralwidget);
+        widget_23->setObjectName("widget_23");
+        widget_23->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_23, 3, 6, 1, 1);
+
+        widget_32 = new QWidget(centralwidget);
+        widget_32->setObjectName("widget_32");
+        widget_32->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_32, 5, 3, 1, 1);
+
+        widget_31 = new QWidget(centralwidget);
+        widget_31->setObjectName("widget_31");
+        widget_31->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_31, 5, 2, 1, 1);
+
+        widget_15 = new QWidget(centralwidget);
+        widget_15->setObjectName("widget_15");
+        widget_15->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_15, 2, 4, 1, 1);
+
+        widget_16 = new QWidget(centralwidget);
+        widget_16->setObjectName("widget_16");
+        widget_16->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_16, 2, 5, 1, 1);
+
+        widget_30 = new QWidget(centralwidget);
+        widget_30->setObjectName("widget_30");
+        widget_30->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_30, 5, 1, 1, 1);
+
+        widget_8 = new QWidget(centralwidget);
+        widget_8->setObjectName("widget_8");
+        widget_8->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_8, 1, 4, 1, 1);
+
+        widget_11 = new QWidget(centralwidget);
+        widget_11->setObjectName("widget_11");
+        widget_11->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_11, 3, 0, 1, 1);
+
+        widget_14 = new QWidget(centralwidget);
+        widget_14->setObjectName("widget_14");
+        widget_14->setStyleSheet(QString::fromUtf8("border-width: 2px;\n"
+"border-style: solid;\n"
+"border-color: black;"));
+
+        gridLayout_2->addWidget(widget_14, 2, 3, 1, 1);
+
+        label_4 = new QLabel(centralwidget);
+        label_4->setObjectName("label_4");
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(label_4->sizePolicy().hasHeightForWidth());
+        label_4->setSizePolicy(sizePolicy);
+        label_4->setFrameShape(QFrame::NoFrame);
+
+        gridLayout_2->addWidget(label_4, 0, 0, 1, 1);
+
+        label_7 = new QLabel(centralwidget);
+        label_7->setObjectName("label_7");
+        sizePolicy.setHeightForWidth(label_7->sizePolicy().hasHeightForWidth());
+        label_7->setSizePolicy(sizePolicy);
+        label_7->setFrameShape(QFrame::NoFrame);
+
+        gridLayout_2->addWidget(label_7, 0, 1, 1, 1);
+
+        label_6 = new QLabel(centralwidget);
+        label_6->setObjectName("label_6");
+        sizePolicy.setHeightForWidth(label_6->sizePolicy().hasHeightForWidth());
+        label_6->setSizePolicy(sizePolicy);
+        label_6->setFrameShape(QFrame::NoFrame);
+
+        gridLayout_2->addWidget(label_6, 0, 2, 1, 1);
+
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName("label_5");
+        sizePolicy.setHeightForWidth(label_5->sizePolicy().hasHeightForWidth());
+        label_5->setSizePolicy(sizePolicy);
+        label_5->setFrameShape(QFrame::NoFrame);
+
+        gridLayout_2->addWidget(label_5, 0, 3, 1, 1);
+
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName("label_3");
+        sizePolicy.setHeightForWidth(label_3->sizePolicy().hasHeightForWidth());
+        label_3->setSizePolicy(sizePolicy);
+        label_3->setFrameShape(QFrame::NoFrame);
+
+        gridLayout_2->addWidget(label_3, 0, 4, 1, 1);
+
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName("label_2");
+        sizePolicy.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
+        label_2->setSizePolicy(sizePolicy);
+        label_2->setFrameShape(QFrame::NoFrame);
+
+        gridLayout_2->addWidget(label_2, 0, 5, 1, 1);
+
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        sizePolicy.setHeightForWidth(label->sizePolicy().hasHeightForWidth());
+        label->setSizePolicy(sizePolicy);
+        label->setFrameShape(QFrame::NoFrame);
+
+        gridLayout_2->addWidget(label, 0, 6, 1, 1);
+
+
+        gridLayout_6->addLayout(gridLayout_2, 2, 0, 2, 8);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 800, 21));
+        menumenu = new QMenu(menubar);
+        menumenu->setObjectName("menumenu");
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+        toolBar = new QToolBar(MainWindow);
+        toolBar->setObjectName("toolBar");
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        menubar->addAction(menumenu->menuAction());
+        toolBar->addAction(actionstrza_ka_w_gore);
+        toolBar->addAction(actionstrza_ka_w_d);
+        toolBar->addAction(actionmiesiac_i_rok);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionstrza_ka_w_gore->setText(QCoreApplication::translate("MainWindow", "strza\305\202ka_w_gore", nullptr));
+        actionstrza_ka_w_d->setText(QCoreApplication::translate("MainWindow", "strza\305\202ka_w_d\303\263\305\202", nullptr));
+        actionmiesiac_i_rok->setText(QCoreApplication::translate("MainWindow", "miesiac_i_rok", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "Poniedzia\305\202ek", nullptr));
+        label_7->setText(QCoreApplication::translate("MainWindow", "Wtorek", nullptr));
+        label_6->setText(QCoreApplication::translate("MainWindow", "\305\232roda", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "Czwartek", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Pi\304\205tek", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Sobota", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Niedziela", nullptr));
+        menumenu->setTitle(QCoreApplication::translate("MainWindow", "menu", nullptr));
+        toolBar->setWindowTitle(QCoreApplication::translate("MainWindow", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
